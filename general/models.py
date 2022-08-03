4e2f3d1b4a25aa868e044208829348f34a844629
Joselo Tabbia <jltabbia@gmail.com>
2022-08-03 10:55:38 -0300

    actualizacion 03/08/2022 11:00

--- general/models.py
+++ general/models.py
@@ -10,3 +10,8 @@ class Localidades(models.Model):
     codigo_localidad=models.IntegerField(verbose_name='Código Localidad')
     nombre_localidad=models.CharField(max_length=100, verbose_name='Localidad')
     
+class Rubros(models.Model):
+    id_rubro=models.IntegerField(verbose_name="Id Rubro")
+    detalle=models.CharField(max_length=100, null=False, verbose_name="Detalle")
+    id_rubro_padre=models.IntegerField()
+    
