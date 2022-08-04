2bb5f10bbafc6532f5f384d9663b035a9288c87c
Joselo Tabbia <jltabbia@gmail.com>
2022-08-04 12:00:35 -0300

    actualizacion 04/08/2022 12:00

--- clientes/views.py
+++ clientes/views.py
@@ -1,8 +1,10 @@
+from asyncio.windows_events import NULL
 import re
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Clientes
 from .forms import ClienteForm
+from general.models import Provincias, Localidades
 
 # Create your views here.
 
@@ -12,11 +14,25 @@ class ClienteHomeView(View):
         return render(request, 'clientes/index.html', {"clientes":clientes})
     
 def crear(request):
-        formulario=ClienteForm(request.POST or None)  
-        if formulario.is_valid():
-            formulario.save()
-            return redirect('clientes:home')
-        return render(request,'clientes/crear.html',{'formulario':formulario})   
+    provincias=Provincias.objects.all()
+    if request.method=="POST":
+        cliente=Clientes()
+        cliente.cuil=request.POST.get('cuil')
+        cliente.nombre=request.POST.get('nombre')
+        cliente.direccion=request.POST.get('direccion')
+        cliente.provincia=request.POST.get('provincia')
+        if request.POST.get('localidad') == NULL:
+            cliente.localidad=24
+        else:
+            cliente.localidad=request.POST.get('localidad')
+        cliente.fecha_alta=request.POST.get('fecha_alta')
+        print(cliente.cuil)
+        cliente.save()
+        return redirect('clientes:home')
+    context={
+        'provincias':provincias,
+    }
+    return render(request,'clientes/crear.html',context)   
 
 def eliminar(request, id):
     cliente=Clientes.objects.get(id=id)
@@ -32,3 +48,7 @@ def editar(request, id):
         return redirect('clientes:home')
     
     return render(request, 'clientes/editar.html',{'formulario':formulario})  
+
+def buscarLoc(request, id):
+    localidades=Localidades.get(id)
+    print(localidades)
