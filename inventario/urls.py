23c92d78545832a295d4f308c32063baf349e1c7
Joselo Tabbia <jltabbia@gmail.com>
2022-08-10 10:07:37 -0300

    Actualizado 10/08/2022 cambio en inventario

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import crear, eliminar, editar, InventarioHomeView
+from .views import CrearView, eliminar, editar, InventarioHomeView
 
 app_name='inventario'
 
 urlpatterns = [
     path('', InventarioHomeView.as_view(), name='home'),
     # path('crear/',crearView.as_view(),name='crear'),
-    path('crear/',crear,name='crear'),
+    path('crear/',CrearView.as_view(),name='crear'),
     path('eliminar/<int:id>', eliminar),
     path('editar/<int:id>',editar)
 
--- inventario/views.py
+++ inventario/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Inventario
 from .forms import InventarioForm
+import datetime
 
 # Create your views here.
 
@@ -10,14 +11,26 @@ class InventarioHomeView(View):
     def get(self, request,*args, **kwargs):
         inventario=Inventario.objects.all()
         return render(request, 'inventario/index.html', {"inventario":inventario})
-    
-def crear(request):
-        formulario=InventarioForm(request.POST or None)  
-        if formulario.is_valid():
-            formulario.save()
-            return redirect('inventario:home')
-        return render(request,'inventario/crear.html',{'formulario':formulario})   
 
+class CrearView(View):
+    def get(self,request,*args, **kwargs):
+        inventario=Inventario.objects.all()
+        context={
+        'inventario':inventario,
+        }
+        return render(request,'inventario/crear.html',context)   
+
+    def post(self,request,*args,**kwargs):
+        inventario=Inventario()
+        inventario.codigo=request.POST.get('codigo')
+        inventario.detalle=request.POST.get('detalle')
+        inventario.stock=request.POST.get('stock')
+        inventario.stock_minimo=request.POST.get('stock_minimo')
+        inventario.ultima_compra=request.POST.get('ultima_compra')
+        inventario.observaciones=request.POST.get('observaciones')
+        inventario.save()
+        return redirect('inventario:home')
+        
 def eliminar(request, id):
     producto=Inventario.objects.get(id=id)
     producto.delete()
@@ -25,10 +38,27 @@ def eliminar(request, id):
     return redirect('inventario:home')
 
 def editar(request, id):
-    inventario=Inventario.objects.get(id=id) 
-    formulario=InventarioForm(request.POST or None, instance=inventario)
-    if formulario.is_valid():
-        formulario.save()
-        return redirect('inventario:home')
     
-    return render(request, 'inventario/editar.html',{'formulario':formulario})  
+    inventario=Inventario.objects.get(id=id) 
+        
+    fecha=inventario.ultima_compra
+    #f=datetime.strftime(fecha, "%Y-%m-%d")
+    print(fecha)
+    
+    if request.method=="POST":
+        inventario.codigo=request.POST.get('codigo')
+        inventario.detalle=request.POST.get('detalle')
+        inventario.stock=request.POST.get('stock')
+        inventario.stock_minimo=request.POST.get('stock_minimo')
+        inventario.ultima_compra=request.POST.get('ultima_compra')
+        inventario.observaciones=request.POST.get('observaciones')
+       
+        inventario.save()
+        return redirect('inventario:home')
+
+    context = {
+        'inventario':inventario,
+        'ultima_compra':datetime.datetime.strftime(fecha,"%Y-%m-%d")
+    }
+    return render(request, 'inventario/editar.html',context)  
+    
